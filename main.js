116a4e0859f2f3344a9e38b1069a4401c8689854
Denys <gozhievd@gmail.com>
2025-04-01 17:19:58 +0200

    mario

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ const cactus = document.getElementById("cactus");
 // const bird = document.getElementById("bird");
 
 document.addEventListener("keydown", function (event) {
-  jump();
+  let keyName = event.key;
+  if (keyName === "ArrowUp" || keyName === " ") {
+    jump();
+  }
+  // if (keyName === "ArrowDown") {
+  //   down();
+  // }
 });
 
 function jump() {
@@ -15,13 +21,22 @@ function jump() {
   }, 350);
 }
 
+// function down() {
+//   if (dino.classList !== "down") {
+//     dino.classList.add("down");
+//   }
+//   else {
+//     dino.classList.remove("down");
+
+// }}
+
 let isAlive = setInterval(function () {
   let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
   let cactusLeft = parseInt(
     window.getComputedStyle(cactus).getPropertyValue("left")
   );
 
-  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+  if (cactusLeft < 90 && cactusLeft > 50 && dinoTop >= 110) {
     alert("You are lose");
   }
 }, 10);
